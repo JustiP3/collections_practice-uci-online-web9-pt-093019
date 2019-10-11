6b894a8130a638dd830440ecc1d8e24ecf0de390
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 22:21:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc (array)
+return array.sort   
+end
+
+def sort_array_desc (array)
+return array.sort {|x,y| -1 * (x <=> y) }  
+end
+
+def sort_array_char_count (array)
+array.sort do |x,y| 
+  if x.length > y.length
+    return 1 
+    elsif x.length == y.length
+    return 0 
+    else
+    return -1 
+    end 
+  end
+
+end
 
