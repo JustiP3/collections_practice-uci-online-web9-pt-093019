1395a0bc6d60bede4abe4c925ed3e14158b58dc8
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 22:28:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,11 @@ def sort_array_char_count (array)
 array.sort {|x,y|  x.length <=> y.length}
 end
 
+def swap_elements (array, index1, index2)
+  placeholder = []
+  placeholder << array[index1]  
+  placeholder << array[index2]
+  array[index1] = placeholder[1]
+  array[index2] = placeholder[0]
+  return array 
+end
