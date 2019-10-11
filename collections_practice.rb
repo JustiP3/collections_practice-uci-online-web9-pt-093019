6804e288dd56cfb1bff1a9daf785c08621758dd9
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 22:51:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc (array)
+return array.sort   
+end
+
+def sort_array_desc (array)
+return array.sort {|x,y| -1 * (x <=> y) }  
+end
+
+def sort_array_char_count (array)
+array.sort {|x,y|  x.length <=> y.length}
+end
+
+def swap_elements (array)
+  placeholder = []
+  placeholder << array[1]  
+  placeholder << array[2]
+  array[1] = placeholder[1]
+  array[2] = placeholder[0]
+  return array 
+end
+
+def reverse_array (array)
+  array.reverse 
+end 
+
+def kesha_maker (array)
+array.each do |x|
+  x[2] = "$"
+end
+end
+
+def find_a (array)
+array.select {|s| s[0] == "a" }
+end
+
+def sum_array(array)
+array.inject {|sum,x| sum + x }
+end
+ 
+def add_s(array)
+  i = 0 
+array.collect do |x|
+  if i != 1 
+    x << "s"
+  end 
+   i += 1  
+  end 
+  array 
+  
+end
+ 
 
