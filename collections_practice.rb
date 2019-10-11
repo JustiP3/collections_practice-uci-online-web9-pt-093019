5a6b05d037d40665bd89f1d06a150d7b59296fcd
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 22:21:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,11 +9,11 @@ end
 def sort_array_char_count (array)
 array.sort do |x,y| 
   if x.length > y.length
-    return 1 
+    1 
     elsif x.length == y.length
-    return 0 
+    0 
     else
-    return -1 
+    -1 
     end 
   end
 
